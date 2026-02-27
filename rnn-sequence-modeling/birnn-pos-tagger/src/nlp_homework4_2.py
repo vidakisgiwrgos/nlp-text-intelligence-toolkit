59816a52a061adea242e1199734cdb37fe32c5b8
Giorgos Vidakis <vidakisgiwrgos@gmail.com>
2026-02-27 17:35:45 +0200

    Update nlp_homework4_2.py
    Cleaned up the code

--- rnn-sequence-modeling/birnn-pos-tagger/src/nlp_homework4_2.py
+++ rnn-sequence-modeling/birnn-pos-tagger/src/nlp_homework4_2.py
@@ -7,13 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1CyDIxanN6xcDXYcdTrhNKKPvwdPKbqX4
 """
 
-!pip install numpy
-!pip install scikit-learn
-!pip install pandas
-!pip install matplotlib
-!pip install umap-learn[plot]
-!pip install pyconll
-
 from statistics import linear_regression
 from sklearn import svm
 from sklearn.model_selection import learning_curve
